25781b4f0fe11f007d5ecb6b0266a6cefe661877
Si Thu Aung <cosmos.stu@gmail.com>
2026-03-02 20:50:48 +0700

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+## Deploy Applications with Kubernetes to the Cloud
+
+### Tests
+```shell
+$ make clean init compile
+$ make gencert
+$ make test
+```
---
+++ cmd/getservers/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	api "github.com/igor-baiborodine/proglog/api/v1"
+)
+
+func main() {
+	addr := flag.String("addr", ":8400", "service address")
+	flag.Parse()
+	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	client := api.NewLogClient(conn)
+	ctx := context.Background()
+	res, err := client.GetServers(ctx, &api.GetServersRequest{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("servers:")
+	for _, server := range res.Servers {
+		fmt.Printf("\t- %v\n", server)
+	}
+}
